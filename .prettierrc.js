cf9ba9140d0bfbec9038f9ae9e8d5fe6e8299ee5
cuiyiming1998 <1269567664@qq.com>
2023-01-03 15:37:54 +0800

    :rocket: chore(*): add eslint commit-lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,11 +6,11 @@ module.exports = {
   singleQuote: true,
   quoteProps: 'as-needed',
   jsxSingleQuote: false,
-  trailingComma: 'es5',
+  trailingComma: 'none',
   bracketSpacing: true,
   jsxBracketSameLine: false,
   arrowParens: 'always',
   htmlWhitespaceSensitivity: 'ignore',
   vueIndentScriptAndStyle: true,
-  endOfLine: 'lf'
+  endOfLine: 'lf',
 }
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,15 @@ export default defineConfig({
     Components({
       resolvers: [AntDesignVueResolver()]
     }),
-    AutoImport({ imports: ['vue', '@vueuse/core', 'pinia', 'vue-router'] }),
+    AutoImport({
+      imports: ['vue', '@vueuse/core', 'pinia', 'vue-router'],
+      dts: './auto-imports.d.ts',
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    }),
     vue(),
     vuejsx()
   ],
